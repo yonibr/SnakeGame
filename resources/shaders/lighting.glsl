054e2fc1ea2e906cacb4e076bbe8ef9304b87bdf
Rubenstein, Yonatan <Yonatan.Rubenstein@viasat.com>
2022-05-11 17:20:57 -0400

    reduced peter panning

--- resources/shaders/lighting.glsl
+++ resources/shaders/lighting.glsl
@@ -73,7 +73,7 @@ float ShadowCalculation(vec4 fragPosLightSpace)
     // calculate bias (based on depth map resolution and slope)
     vec3 normal = fs_in.Normal;
     vec3 lightDir = normalize(lightPos.val - fs_in.FragPos);
-    float bias = max(0.01 * (1.0 - dot(normal, lightDir)), 0.005);
+    float bias = max(0.0075 * (1.0 - dot(normal, lightDir)), 0.0025);
     // check whether current frag pos is in shadow
     // float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
     // PCF
